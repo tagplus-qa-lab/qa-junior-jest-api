369b6ddae3878391c5fb864c442a45c4b20d9782
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2025-10-25 21:30:15 -0300

    fix(api-3): Corrige estrutura do commentSchema

--- tests/schemas/commentSchema.ts
+++ tests/schemas/commentSchema.ts
@@ -1,15 +1,12 @@
 export const commentSchema = {
-  type: 'object',
-  items: {
-    type: 'object',
-    properties: {
-      id: { type: 'number' },
-      post_id: { type: 'number' },
-      name: { type: 'string' },
-      email: { type: 'string'},
-      body: { type: 'string' }
-    },
-    required: ['id', 'post_id', 'name', 'email', 'body'],
-    additionalProperties: false
-  }
+  type: "object",
+  properties: {
+    id: { type: "number" },
+    post_id: { type: "number" },
+    name: { type: "string" },
+    email: { type: "string" },
+    body: { type: "string" }
+  },
+  required: ["id", "post_id", "name", "email", "body"],
+  additionalProperties: false
 };
